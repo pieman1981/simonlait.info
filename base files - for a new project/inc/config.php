0a3b833ef971433f5bc3b36b3874f40bf700b036
Simon Lait <simonlait@gmail.com>
2013-11-13 21:45:38 +0000

    base file ready to start any new project

---
+++ base files - for a new project/inc/config.php
@@ -0,0 +1,6 @@
+<?php
+date_default_timezone_set('Europe/London');
+session_start();
+$g_sSitePath = 'http://localhost/BrochureSite/';
+
+?>
---
+++ base files - for a new project/index.php
@@ -0,0 +1,29 @@
+<?php include 'inc/config.php' ?>
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>Premium WordPress Themes | Wedding WordPress Theme</title>
+  <meta name="description" content="The HTML5 Herald">
+  <meta name="author" content="ThemeName">
+
+  <link rel="stylesheet" href="<?php echo $g_sSitePath ?>style/reset.css">
+  <link rel="stylesheet" href="<?php echo $g_sSitePath ?>style/general.css">
+  <link rel="stylesheet" href="<?php echo $g_sSitePath ?>style/main.css">
+
+  <!--[if lt IE 9]>
+  <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+</head>
+
+<body>
+	<div class="container rounded boxshadow">
+	
+	
+	</div>
+  <script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+  <script src="<?php echo $g_sSitePath ?>js/scripts.js"></script>
+</body>
+</html>
